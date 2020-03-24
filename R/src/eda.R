1259bf262a327afad3c3beabf9d813f48dbebb05
Regina Galambos <gina.galambos@gmail.com>
2020-03-24 12:54:25 +0100

    update README with links to additional datasets

---
+++ R/src/eda.R
@@ -0,0 +1,56 @@
+####################################################################################################
+#                              Praxisprojekt - WiSe 2019/20                                        #
+#         Analysing Coronavirus: Development, reluts in actions/policies, etc.(?)                  #
+#                         Regina Galambos, Lorenz Mihatsch                                         #
+####################################################################################################
+
+######################################### Preparation #############################################
+
+# Set working directory
+setwd("./")
+
+#....................................... Load packages ..............................................
+library(dplyr)
+library(tidyr)
+
+#.................................. Load data from package ..........................................
+library(coronavirus)
+data("coronavirus")
+
+####################################### Inspect dataset ############################################
+
+# copy dataset to working data frame
+df <- coronavirus
+
+# convert Province.State, Country.State, type from character to factor
+df <- mutate_if(df, is.character, as.factor)
+
+str(df)
+summary(df)
+
+missing_col <- sapply(df, function(x) sum(is.na(x))) / nrow(df)
+missing_col[missing_col > 0]
+
+###################################### Summary of dataset ##########################################
+
+df %>%
+  group_by(Country.Region, type) %>%
+  summarise(total_cases = sum(cases)) %>%
+  arrange(total_cases)
+
+
+df %>% 
+  filter(date == max(date)) %>%
+  select(country = Country.Region, type, cases) %>%
+  group_by(country, type) %>%
+  summarise(total_cases = sum(cases)) %>%
+  pivot_wider(names_from = type, values_from = total_cases) %>%
+  arrange(-confirmed)
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 Dataset: https://github.com/RamiKrispin/coronavirus
 
+Additional public datasets:
+
+- global: https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
+- Germany: https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0/data
+
 **Ideas:**
 
 - Describe data with maps
-- Inspect trends in different countries global/Germany/... (death rates, etc)
+- Inspect trends in different countries **global**/Germany/... (death rates, etc)
 - Economics
 - Data about different countries form UN
 - Check for anomalies
