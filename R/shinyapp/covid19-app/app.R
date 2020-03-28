38b27d4d79c963784833553b3113566e503dbb06
Regina Galambos <gina.galambos@gmail.com>
2020-03-28 23:58:39 +0100

    add plotly plot with update on user input

--- R/shinyapp/covid19-app/app.R
+++ R/shinyapp/covid19-app/app.R
@@ -106,6 +106,40 @@ server <- function(input, output, session) {
     updateSelectInput(session, "country", choices=countries, 
                       selected="Italy")
     
+    # Plot
+    renderBarPlot = function(varPrefix, legendPrefix, yaxisTitle) {
+        renderPlotly({
+            data = data()
+            plt = data %>% 
+                plot_ly() %>%
+                config(displayModeBar=FALSE) %>%
+                layout(
+                    #barmode='group', 
+                    xaxis=list(
+                        title="", tickangle=-90, type='category', ticktext=as.list(data$dateStr), 
+                        tickvals=as.list(data$date), gridwidth=1), 
+                    yaxis=list(
+                        title=yaxisTitle
+                    ),
+                    legend=list(x=0.05, y=0.95, font=list(size=15), bgcolor='rgba(240,240,240,0.5)')#,
+                    #font=f1
+                )
+            for(type in input$types) 
+                plt = plt %>%
+                add_trace(
+                    x= ~date, y=data[[paste0(varPrefix, type)]], type='bar', 
+                    name=paste(legendPrefix, type, "Cases"),
+                    marker=list(
+                        color=switch(type, Deaths='rgb(200,30,30)', Recovered='rgb(30,200,30)', Confirmed='rgb(100,140,240)'),
+                        line=list(color='rgb(8,48,107)', width=1.0)
+                    )
+                )
+            plt
+        })
+    }
+    #output$dailyMetrics = renderBarPlot("New", legendPrefix="New", yaxisTitle="New Cases per Day")
+    output$cumulativeMetrics = renderBarPlot("Cum", legendPrefix="Cumulated", yaxisTitle="Cumulated Cases")
+    
 }
 
 
